52df9b33702c1146275b01c4551a0855f3785a33
aitsystemsgit <abdo.fahmy9696@gmail.com>
2022-12-25 02:28:39 +0200

    fix issue 2

--- src/views/Auth/Login.vue
+++ src/views/Auth/Login.vue
@@ -148,22 +148,46 @@ import TargetComponent from '../../components/TargetComponent.vue';
                 /*
                     public function store(Request $request)
                     {
+                        $role = 'finance';
+                        $now = Carbon::now();
+                        $month = $now->month;
                         $data = $request->all();
                         for($i = 0 ; $i < count($data) ; $i++){
-                            Target::updateOrCreate(
-                                [
-                                    'month' => $data[$i]['month'],
-                                    'cluster_id' => $data[$i]['cluster_id']
-                                ],
-                                [
-                                    'month' => $data[$i]['month'],
-                                    'target' => $data[$i]['target'],
-                                    'year' => $data[$i]['year'],
-                                    'cluster_id' => $data[$i]['cluster_id'],
-                                ]
-                        );
+                            if($role === 'finance'){
+                                if($data[$i]['month'] === $month){
+                                    Target::updateOrCreate(
+                                        [
+                                            'month' => $data[$i]['month'],
+                                            'cluster_id' => $data[$i]['cluster_id']
+                                        ],
+                                        [
+                                            'month' => $data[$i]['month'],
+                                            'target' => $data[$i]['target'],
+                                            'year' => $data[$i]['year'],
+                                            'cluster_id' => $data[$i]['cluster_id'],
+                                        ]
+                                    );
+                                }else{
+                                    return response()->json(['message' => 'You cant store in this month']);
+                                }
+                            }else{
+                                Target::updateOrCreate(
+                                    [
+                                        'month' => $data[$i]['month'],
+                                        'cluster_id' => $data[$i]['cluster_id']
+                                    ],
+                                    [
+                                        'month' => $data[$i]['month'],
+                                        'target' => $data[$i]['target'],
+                                        'year' => $data[$i]['year'],
+                                        'cluster_id' => $data[$i]['cluster_id'],
+                                    ]
+                                );
+                            }
                         }
                         return response()->json(['message' => 'Targets Saved Successfully.']);
+                        
+                    }
                     }
                 */
             }
